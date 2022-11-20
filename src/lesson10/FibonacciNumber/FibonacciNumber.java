14b640ecf9ddc8877705cf45ec993407eb9313ff
Vadim Arefiev <vadimarefev@i.ua>
2022-11-20 18:59:39 +0200

    final version

--- src/lesson10/FibonacciNumber/FibonacciNumber.java
+++ src/lesson10/FibonacciNumber/FibonacciNumber.java
@@ -4,14 +4,20 @@ import java.util.Scanner;
 
 public class FibonacciNumber {
 
+    /**
+     * The Fibonacci numbers uniquely denoted by the F(n) form of the sequence are called the Fibonacci sequence
+     so that all numbers are the sum of two lavi ones, starting from 0 and 1. That is,
+     F(0) = 0, F(1) = 1
+     F(n) = F(n – 1) + F(n – 2), for n > 1.
+    */
+
     public static void main(String[] args) {
-        System.out.println("enter a number from 0 to 30:");
+        System.out.println("enter a number from 2 to 30:");
         Scanner scanner = new Scanner(System.in);
         fib(scanner.nextInt());
     }
 
     public static void fib(int n) {
-
         int[] fib = new int[n];
         fib[0] = 0;
         fib[1] = 1;
